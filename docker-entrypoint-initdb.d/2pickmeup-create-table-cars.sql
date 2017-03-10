cdf43069edad4f5dd11be87e265c574bb5f7cd38
physelvasics <physelvasics@gmail.com>
2017-03-09 20:54:03 -0600

    update

--- docker-entrypoint-initdb.d/2pickmeup-create-table-cars.sql
+++ docker-entrypoint-initdb.d/2pickmeup-create-table-cars.sql
@@ -1,7 +1,21 @@
-CREATE TABLE IF NOT EXISTS cars (
-   car_id   NUMERIC NOT NULL,
-   user_id NUMERIC NOT NULL REFERENCES users (user_id),
-   make VARCHAR(100),
-   occupancy NUMERIC NOT NULL,   
-   CONSTRAINT cars_pk PRIMARY KEY (car_id)
-);
+-- Table: public.cars
+
+-- DROP TABLE public.cars;
+
+CREATE TABLE public.cars
+(
+    user_id integer NOT NULL,
+    car_id integer NOT NULL,
+    make character varying(255) COLLATE pg_catalog."default",
+    color character varying(255) COLLATE pg_catalog."default",
+    model character varying(255) COLLATE pg_catalog."default",
+    occupancy integer,
+    CONSTRAINT cars_pkey PRIMARY KEY (user_id, car_id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.cars
+    OWNER to postgres;
