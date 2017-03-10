c9798104856ca93921bb96b05bf76136fd6ee984
physelvasics <physelvasics@gmail.com>
2017-03-09 20:55:19 -0600

    update

--- docker-entrypoint-initdb.d/1pickmeup-create-table-users.sql
+++ docker-entrypoint-initdb.d/1pickmeup-create-table-users.sql
@@ -1,11 +1,23 @@
-CREATE TABLE IF NOT EXISTS users (
-   user_id   NUMERIC       NOT NULL,
-   user_password VARCHAR(25) NOT NULL,
-   first_name    VARCHAR(25) NOT NULL,
-   last_name     VARCHAR(25) NOT NULL,
-   date_of_birth DATE NOT NULL,
-   phone_number  VARCHAR(15) NOT NULL,
-   email          CHAR(150) NOT NULL,
-   address          CHAR(500) NOT NULL,
-   CONSTRAINT users_pk PRIMARY KEY (user_id)
-);
+-- Table: public.users
+
+-- DROP TABLE public.users;
+
+CREATE TABLE public.users
+(
+    user_id numeric NOT NULL,
+    user_password character varying(25) COLLATE pg_catalog."default" NOT NULL,
+    first_name character varying(25) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(25) COLLATE pg_catalog."default" NOT NULL,
+    date_of_birth date NOT NULL,
+    phone_number character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    email character(150) COLLATE pg_catalog."default" NOT NULL,
+    address character(500) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (user_id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to postgres;
